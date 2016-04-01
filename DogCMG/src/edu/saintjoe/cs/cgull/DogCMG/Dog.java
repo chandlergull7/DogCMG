8c11feefa57fdb9f10b62cf1f8b6f70498d7caba
cgull <cgull@localhost>
2016-03-31 21:23:09 -0400

    Dog class

--- DogCMG/src/edu/saintjoe/cs/cgull/DogCMG/Dog.java
+++ DogCMG/src/edu/saintjoe/cs/cgull/DogCMG/Dog.java
@@ -1,10 +1,17 @@
 package edu.saintjoe.cs.cgull.DogCMG;
 
 public class Dog {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	int size;
+	String name;
+	
+	void bark() {
+		if (size > 60) {
+			System.out.println("Wooof! Wooof!");
+		} else if (size > 14) {
+			System.out.println("Ruff! Ruff!");
+		} else {
+			System.out.println("Yip! Yip!");
+			
+		}
 	}
-
 }
